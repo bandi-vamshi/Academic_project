53cb283590a5369ca207e1d76d43e216ad1587c8
vamshi <vamshibandi520@gmail.com>
2024-06-21 22:14:15 +0530

    first commit

---
+++ jsfile.js
@@ -0,0 +1,138 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "white",
+        img: "./img/img1.png",
+      },
+      {
+        code: "black",
+        img: "./img/imgb6.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "red",
+        img: "./img/imgr7.png",
+      },
+      {
+        code: "green",
+        img: "./img/img2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "black",
+        img: "./img/img3.png",
+      },
+      {
+        code: "white",
+        img: "./img/blazer.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./img/img4.png",
+      },
+       {
+         code: "lightgray",
+         img: "./img/imgw9.png",
+
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "brown",
+        img: "./img/img5.png",
+      },
+      {
+        code: "blue",
+        img: "./img/imgb10.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+  
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+
+    choosenProduct = products[index];
+
+
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
---
+++ login.js
@@ -0,0 +1,66 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+const login_btn = document.querySelector("#nav")
+
+sign_up_btn.addEventListener('click', () =>{
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener('click', () =>{
+    container.classList.remove("sign-up-mode");
+});
+
+let infodetails=[];
+function signUp() {
+    var username = document.getElementById('sign-user').value;
+    var password = document.getElementById('sign-pwd').value;
+    var email = document.getElementById('sign-email').value;
+
+    var user = {
+        user: username,
+        pwd: password,
+        mail: email
+    };
+    
+    infodetails.push(user);
+    for (x of infodetails)
+        console.log(x)
+
+    localStorage.setItem('user', JSON.stringify(user));
+
+    console.log(user)
+    console.log('User signed up:', user);
+    alert('Sign up successful!');
+}
+
+
+
+
+function signIn() {
+    var enteredemail = document.getElementById('email').value;
+    var enteredPassword = document.getElementById('password').value;
+
+    var storedUser = JSON.parse(localStorage.getItem('user'));
+
+    if (storedUser) {
+        if (storedUser.mail == enteredemail && storedUser.pwd == enteredPassword) {
+            console.log('Sign in successful');
+            alert('Sign in successful!');
+       
+            window.open("htmlfile.html");
+        } else {
+            console.log('Invalid username or password');
+            alert('Invalid username or password');
+        }
+    } else {
+        console.log('No user found. Please sign up first.');
+        alert('No user found. Please sign up first.');
+    }
+}
+
+
+
+
+
+
